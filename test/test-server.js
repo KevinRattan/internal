7523ca17805718837d3cd6dfb989b7f49e20ac2c
Kevin <rattan.kevin.k@gmail.com>
2020-08-21 07:22:02 +0000

    updated version

--- test/test-server.js
+++ test/test-server.js
@@ -23,7 +23,7 @@ describe('GET /', function() {
           if (err) {
             return done(err);
           }
-          chai.expect(JSON.parse(res.text)).to.eql({ version: '1.0.10' });
+          chai.expect(JSON.parse(res.text)).to.eql({ version: '1.0.11' });
           return done();
         });
     });
